cf1eb9c5be8d3de3e46128f3cef750dc61cbaed3
judhenaoma <judhenaoma@unal.edu.co>
2023-03-27 10:23:27 -0500

    fixing urls

--- bicimaps/urls.py
+++ bicimaps/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('admin/', admin.site.urls),
     path('api/', include('bicimaps_app.api.urls')),
 ]
